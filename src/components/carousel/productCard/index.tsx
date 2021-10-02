dff48ebc7cd2fd2c2b77a1e51228ceef61819eac
reantepattersondev <reantepattersondev@outlook.com>
2021-10-02 09:52:27 -0500

    fix(svg): color update

--- src/components/carousel/productCard/index.tsx
+++ src/components/carousel/productCard/index.tsx
@@ -73,15 +73,14 @@ const ProductCard: React.FunctionComponent<ProductTypesense> = (
                         >
                             {!isStockShown && (
                                 <svg
-                                    xmlns="http://www.w3.org/2000/svg"
                                     className="h-17p w-17p fill-current my-auto"
                                     viewBox="0 0 20 20"
-                                    fill="currentColor"
+                                    fill="#673545"
+                                    xmlns="http://www.w3.org/2000/svg"
                                 >
                                     <path
-                                        fillRule="evenodd"
                                         d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                                        clipRule="evenodd"
+                                        fill="#673545"
                                     />
                                 </svg>
                             )}
@@ -92,16 +91,15 @@ const ProductCard: React.FunctionComponent<ProductTypesense> = (
                             )}
                             {isStockShown && (
                                 <svg
-                                    xmlns="http://www.w3.org/2000/svg"
                                     className="h-17p w-17p fill-current my-auto"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
+                                    viewBox="0 0 32 32"
+                                    fill="#673545"
+                                    xmlns="http://www.w3.org/2000/svg"
                                 >
                                     <path
-                                        fillRule="evenodd"
-                                        d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                                        clipRule="evenodd"
-                                    />
+                                        d="M21.3 29.28h-4.84v-7.29a9.77 9.77 0 004.33-1.8 8.28 8.28 0 003.4-6.94c0-4.92-1.52-9.35-2.17-11.02A1.87 1.87 0 0020.27 1h-9.5c-.79 0-1.5.5-1.77 1.26-.6 1.68-2 6.1-2 11 0 3.65 1.85 5.8 3.4 6.94a9.84 9.84 0 004.4 1.8v7.28H9.97a.85.85 0 00-.84.86c0 .47.38.86.84.86H21.3c.46 0 .83-.39.83-.86a.85.85 0 00-.83-.86zM10.56 2.86a.22.22 0 01.2-.14h9.51c.1 0 .17.05.2.14a32.1 32.1 0 011.62 5.77H9.07a33.8 33.8 0 011.5-5.77zm4.91 17.47c-.7 0-2.47-.32-4.1-1.52a6.54 6.54 0 01-2.7-5.56c0-.99.06-1.96.16-2.9h13.51c.1.93.18 1.9.18 2.9 0 2.36-.91 4.23-2.7 5.56a7.67 7.67 0 01-4.1 1.52h-.25zM24.04 31c.53 0 .96-.44.96-.99a.97.97 0 00-.96-.98.97.97 0 00-.96.98c0 .55.43.99.96.99z"
+                                        fill="#673545"
+                                    ></path>
                                 </svg>
                             )}
                             {isStockShown && (
